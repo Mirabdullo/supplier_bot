5903090bd20d9d536539089843fdfb2d48e56a6d
Mahmudov Mirabdullo <mmirabdullo1999@gmail.com>
2023-08-29 17:44:27 +0500

    change token

--- src/core/bot.ts
+++ src/core/bot.ts
@@ -5,8 +5,8 @@ import cron from "node-cron"
 import { User } from "../models/user.model";
 import { newProducts } from "../libs/products.service";
 
-// const token = "6491679680:AAFaZfYkSjdP3bY62VBzY57u8Uzt7exMTMY";   // server token
-const token = "6243689515:AAHok9fMY6ef83GHkCCUJvMyhHFL_JUSt2I"   //local token
+const token = "6491679680:AAFaZfYkSjdP3bY62VBzY57u8Uzt7exMTMY";   // server token
+// const token = "6243689515:AAHok9fMY6ef83GHkCCUJvMyhHFL_JUSt2I"   //local token
 
 export const bot = new Telegraf(token);
 
