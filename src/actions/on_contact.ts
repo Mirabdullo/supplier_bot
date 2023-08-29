3fa339cecac39886c55a0be6854dc5d066066a3a
Mahmudov Mirabdullo <mmirabdullo1999@gmail.com>
2023-08-29 15:02:59 +0500

    change auth

--- src/actions/on_contact.ts
+++ src/actions/on_contact.ts
@@ -3,42 +3,56 @@ import { User } from "../models/user.model";
 import { bot } from "../core";
 import { newProducts } from "../libs/products.service";
 
-const composer = new Composer()
+const composer = new Composer();
 
-composer.on('contact', async (ctx) => {
-    const contact = ctx.message.contact
-    let num = contact.phone_number.trim().replace(" ", "")
+composer.on("contact", async (ctx) => {
+    const contact = ctx.message.contact;
+    let num = "+" + contact.phone_number.trim().replace(" ", "");
+    let telegramId = ctx.message.contact.user_id
+    console.log(contact, num);
 
-    const user = await User.findOne({ where: { phone: num } })
+    const user = await User.findOne({ where: { phone: num } });
     if (!user) {
-        await ctx.reply(`Неправильный номер.\nВведите свой контактный номер которое привязан  к <b>Woodline</b>, как показано в образце!\n\Образец: +998951234567`, {
-            parse_mode: "HTML",
-            reply_markup: { remove_keyboard: true }
-        });
-        
-    }
-    else if (user && "role" in user) {
-        if (user.role !== "PRODUCER") {
-            await ctx.reply(`Извините, вы не можете использовать этого бота!`, {
-                parse_mode: "HTML"
-            })
-        }
-
+        await ctx.reply(
+            `Неправильный номер.\nВведите свой контактный номер которое привязан  к <b>Woodline</b>, как показано в образце!\n\Образец: +998951234567`,
+            {
+                parse_mode: "HTML",
+                reply_markup: { remove_keyboard: true },
+            }
+        );
     } else {
-        if ("use_bot" in user) { 
-            if(user.use_bot){
-                await newProducts(ctx, user.dataValues.comp_id)
-            } else {
-                await ctx.reply(`Номер принят. Для использования бота обратитесь к <a href="https://t.me/Fatkhull01">администратору</a> Woodline.\n\nМы рады работать с вами!`, {
+        let role = user?.dataValues?.role;
+        let use = user?.dataValues?.use_bot
+        let bot_id = user?.dataValues?.bot_id
+
+        if (role !== "PRODUCER") {
+            await ctx.reply(`Извините, вы не можете использовать этого бота!`, {
+                parse_mode: "HTML",
+            });
+        } else {
+            if (use && bot_id === telegramId) {
+                await newProducts(ctx, user.dataValues.comp_id);
+            } else if (use && bot_id !== telegramId) {
+                await ctx.reply("Неверный аккаунт Telegram!", {
                     parse_mode: "HTML"
                 })
+            } else {
+                await ctx.reply(
+                    `Номер принят. Для использования бота обратитесь к <a href="https://t.me/Fatkhull01">администратору</a> Woodline.\n\nМы рады работать с вами!`,
+                    {
+                        parse_mode: "HTML",
+                    }
+                );
+
+                if (!user.dataValues.bot_id) {
+                    await User.update({ bot_id: ctx.from.id }, { where: { id: user.dataValues.id } });
+                }
             }
         }
-        let id = user.dataValues.comp_id
-        await newProducts(ctx, id)
-    } 
 
-})
+        // let id = user.dataValues.comp_id;
+        // await newProducts(ctx, id);
+    }
+});
 
-
-bot.use(composer.middleware())
+bot.use(composer.middleware());
--- src/actions/on_message.ts
+++ src/actions/on_message.ts
@@ -7,36 +7,40 @@ const composer = new Composer();
 
 composer.on("message", async (ctx) => {
     try {
+        let telegramId = ctx.from.id
         if ("text" in ctx.update.message) {
             console.log(ctx.update.message.text);
             const user = await User.findOne({ where: { phone: ctx.update.message.text } });
             if (user) {
                 console.log(user.dataValues);
                 let id = user.dataValues.comp_id;
-                if ("use_bot" in user) {
-                    if (user.use_bot) {
-                        await newProducts(ctx, id);
-                    } else {
-                        await ctx.reply(
-                            "Номер принят. Для использования бота обратитесь к администратору Woodline.\n\nПриносим извинения за неудобства!",
-                            {
-                                parse_mode: "HTML",
-                            }
-                        );
+                let role = user.dataValues.role
+                let bot_id = user.dataValues.bot_id
 
-                        if (!user.dataValues.bot_id) {
-                            await User.update({ bot_id: ctx.from.id }, { where: { id: user.dataValues.id } });
-                        }
-                    }
-                }
-            }
-            else if (user && "role" in user) {
-                if (user["role"] !== "PRODUCER") {
+                if (role !== "PRODUCER") {
                     await ctx.reply(`Извините, вы не можете использовать этого бота!`, {
                         parse_mode: "HTML"
                     })
                 }
+                else {
+                    let use = user.dataValues.use_bot
+                        if (use && telegramId === bot_id) {
+                            await newProducts(ctx, id);
+                        } else {
+                            await ctx.reply(
+                                "Номер принят. Для использования бота обратитесь к администратору Woodline.\n\nПриносим извинения за неудобства!",
+                                {
+                                    parse_mode: "HTML",
+                                }
+                            );
+    
+                            if (!user.dataValues.bot_id) {
+                                await User.update({ bot_id: ctx.from.id }, { where: { id: user.dataValues.id } });
+                            }
+                        }
+                }
             }
+
             else {
                 console.log("user null");
                 await ctx.reply(
