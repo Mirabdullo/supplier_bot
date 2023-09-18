78b96dd34ac24002d14b779f0f531e07d9f3d910
Mahmudov Mirabdullo <mmirabdullo1999@gmail.com>
2023-09-18 12:26:46 +0500

    add create warehouse on accept product

--- src/core/bot.ts
+++ src/core/bot.ts
@@ -2,8 +2,8 @@ import { Context, Telegraf } from "telegraf";
 import { sequelize } from "./db"
 
 
-// const token = "6491679680:AAFaZfYkSjdP3bY62VBzY57u8Uzt7exMTMY";   // server token
-const token = "6243689515:AAHok9fMY6ef83GHkCCUJvMyhHFL_JUSt2I"   //local token
+const token = "6491679680:AAFaZfYkSjdP3bY62VBzY57u8Uzt7exMTMY";   // server token
+// const token = "6243689515:AAHok9fMY6ef83GHkCCUJvMyhHFL_JUSt2I"   //local token
 
 export const bot = new Telegraf(token);
 
